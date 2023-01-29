2936a003a4b745c1fac958e16ae539691aeaabb6
c-swan <carl.swanberg.1423@student.uu.se>
2023-01-29 21:46:52 +0100

    Game Logic, Snowball, Input Fixes, Scrolling Fixes, Spawning Algorithm Adjustments
    Snowball, Input Fixes, Scrolling Fixes, Spawning Algorithm Adjustments
    
    Pretty much only need to fix collision, right now collision detections don't work which makes it difficult to test the game logic I implemented.

--- Assets/ScrollingObject.cs
+++ Assets/ScrollingObject.cs
@@ -9,11 +9,10 @@ public class ScrollingObject : MonoBehaviour
     public const int col_count = 12;
     public const int row_count = 5;
 
-    public static float[] d_xpos = { 8.493f, 6.83f, 5.16f, 3.51f, 1.84f, 0.17f, -1.5f, -3.16f, -4.83f, -6.1f, -6.1f, -9.05f };        //column -> y position (right to left)
-    public static float[] d_ypos = { 3.028f, 1.172f, -0.704f, -2.563f, -4.418f };      //rows -> x position (top to bottom)
+    public static float[] d_xpos = { 8.5f, 6.83f, 5.16f, 3.5f, 1.83f, 0.17f, -1.5f, -3.17f, -4.83f, -6.5f, -8.17f, -9.05f };        //column -> y position (right to left)
+    public static float[] d_ypos = { 3.03f, 1.17f, -0.7f, -2.56f, -4.42f };      //rows -> x position (top to bottom)
     
     float t_counter = 0;
-
     int row = 0;    //0-4, initialized by spawn
     int col = 0;    //far right
 
@@ -29,8 +28,7 @@ public class ScrollingObject : MonoBehaviour
 
     public void spawn(int r)
     {
-        if(r >= row_count || r < 0) Debug.Log("invalid row: " + r);
-        else row = r;
+        row = Mathf.Clamp(r, 0, 4);
 
         transform.position = new Vector3(d_xpos[col] + offset.x, d_ypos[row] + offset.y, 0);
         if(sr != null) sr.enabled = true;
@@ -51,13 +49,15 @@ public class ScrollingObject : MonoBehaviour
     {
         col++;
         
-        if(col == 9 || col == 10) sr.enabled = false;
-        else if(col == (col_count - 1)) sr.enabled = true;
-
+        if(tag != "Wall")
+        {
+            if(col == 9 || col == 10) sr.enabled = false;   //hide but don't disable
+            else if(col == (col_count - 1)) sr.enabled = true;  //show again
+        }
+        
         if(col >= col_count)
             Destroy(gameObject);    //off screen - out of sight out of mind
         else
-            transform.position = new Vector3(d_xpos[col % col_count] + offset.x, transform.position.y + offset.y, 0);  // move to next position
+            transform.position = new Vector3(d_xpos[col % col_count] + offset.x, transform.position.y, 0);  // move to next position
     }
-
 }
